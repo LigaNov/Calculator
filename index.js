bc5b9fcc880e234c3df963bbadba923bc164a85d
LigaNov <liiga.novika@gmail.com>
2022-05-01 22:33:16 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function calcNumbers(result){
+  allCalc.inputField.value=allCalc.inputField.value+result;
+}
+
+function clearInput(){ //resets all entered values
+  allCalc.inputField.value="";
+}
+
+
+//todo explore how to add validation!
